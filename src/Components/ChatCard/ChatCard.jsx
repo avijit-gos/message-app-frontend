4ce68071d4a46ac9134b7678d47cd9791e9b05dc
unknown <avijit-gos>
2024-02-17 22:28:28 +0530

    Channel UI & Chat card added

--- src/Components/ChatCard/ChatCard.jsx
+++ src/Components/ChatCard/ChatCard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+/** @format */
 
-const chatCard = ({chat}) => {
+import { Avatar, Box } from "@chakra-ui/react";
+import React from "react";
+import "./ChatCard.css";
+import { getChatName } from "../../Utils/getChatName";
+
+const chatCard = ({ chat }) => {
+  const getData = getChatName(
+    chat.users,
+    JSON.parse(localStorage.getItem("user"))
+  );
   return (
-    <div>chatCard</div>
-  )
-}
+    <>
+      {chat.isGroup ? (
+        <Box className='chat_card'>GROUP</Box>
+      ) : (
+        <Box className='chat_card'>
+          {/* Chat avatar */}
+          <Avatar src={getData.p_i} className="chat_avatar" />
+          {/* chat info */}
+          <Box className='chat_card_info'>
+            <Box className='chat_name'>{getData.name}</Box>
+            <Box className='chat_message'>Sample latest message</Box>
+          </Box>
+        </Box>
+      )}
+    </>
+  );
+};
 
-export default chatCard
+export default chatCard;
--- src/Pages/Private/Home/Home.jsx
+++ src/Pages/Private/Home/Home.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import React, { useState, useEffect, useLayoutEffect } from "react";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, useToast } from "@chakra-ui/react";
 import "./Home.css";
 import { GlobalContext } from "../../../Context/Context";
 import Layout from "../../../Layout/Layout";
@@ -9,10 +9,17 @@ import Chat from "./Components/Chat/Chat";
 import Message from "./Components/Message/Message";
 import InterestModal from "../../../Components/ModalComp/InterestModal";
 import Interests from "../../../Config/interests.json";
+import AuthButton from "../../../Components/ButtonComp/AuthButton";
+import axios from "axios";
+
 const Home = () => {
+  const toast = useToast();
   const { setPageType } = GlobalContext();
   const user = JSON.parse(localStorage.getItem("user"));
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const [hobbies, setHobbies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [disable, setDisable] = useState(true);
 
   useLayoutEffect(() => {
     setPageType("home");
@@ -25,6 +32,70 @@ const Home = () => {
       setIsOpenModal(true);
     }
   }, []);
+
+  const handleAddHobies = (data) => {
+    if (!hobbies.includes(data.title)) {
+      setHobbies((prev) => [...prev, data.title]);
+    } else {
+      const temp = hobbies;
+      const arr = temp.filter((i) => i !== data.title);
+      setHobbies(arr);
+    }
+  };
+
+  useEffect(() => {
+    if (!hobbies.length) {
+      setDisable(true);
+    } else {
+      setDisable(false);
+    }
+  }, [hobbies]);
+
+  const handleUpdateInterest = () => {
+    setDisable(true);
+    setLoading(true);
+    let data = JSON.stringify({
+      hobbies: hobbies,
+    });
+
+    let config = {
+      method: "put",
+      maxBodyLength: Infinity,
+      url: `${process.env.REACT_APP_BASE_URL}api/user/update/profile/interest`,
+      headers: {
+        "x-access-token": localStorage.getItem("token"),
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+        toast({
+          title: "Success",
+          description: `${response.data.msg}`,
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+        setLoading(false);
+        setIsOpenModal(false);
+      })
+      .catch((error) => {
+        console.log(error.response.data.error.message);
+        toast({
+          title: "Error",
+          description: `${error.response.data.error.message}`,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+        setLoading(false);
+      });
+  };
+
   return (
     <Layout>
       <Box className='home_container'>
@@ -35,13 +106,31 @@ const Home = () => {
             body={
               <Box className='interest_modal_body'>
                 {Interests.map((data) => (
-                  <Button key={data.title} className='interest_btn'>
+                  <Button
+                    key={data.title}
+                    className={
+                      hobbies.includes(data.title)
+                        ? "interest_btn active_interest_btn"
+                        : "interest_btn"
+                    }
+                    onClick={() => handleAddHobies(data)}>
                     {data.title}
                   </Button>
                 ))}
               </Box>
             }
-            footer={<Box className='interest_modal_footer'>FOOTER</Box>}
+            footer={
+              <Box className='interest_modal_footer'>
+                <AuthButton
+                  loading={loading}
+                  disable={disable}
+                  text={"Update"}
+                  className={"upadte_btn"}
+                  disableClassName={"disable_update_btn"}
+                  clickHandler={handleUpdateInterest}
+                />
+              </Box>
+            }
           />
         )}
         <Chat />
