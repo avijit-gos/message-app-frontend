c0c94dbe36d870d9aaadcc4c47e62b86f04b6015
unknown <avijit-gos>
2024-02-19 12:34:44 +0530

    Adding groups chat comment

---
+++ src/Pages/Private/Home/Components/Chat/Components/Groups.jsx
@@ -0,0 +1,36 @@
+/** @format */
+
+import { Box, Button } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+
+const Groups = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [loading, setLoading] = useState(false);
+  const [active, setActive] = useState();
+
+  const handleChangeSortTab = (data) => {
+    setLoading(true);
+    setActive(data);
+  };
+
+  return (
+    <Box className='channel_container'>
+      {/* Channel sort section */}
+      <Box className='channel_sorting_container'>
+        {user.hobbies.map((data) => (
+          <Button
+            onClick={() => handleChangeSortTab(data)}
+            key={data}
+            className={
+              data === active ? "active_sort_btn sort_btn" : "sort_btn"
+            }>
+            {data}
+          </Button>
+        ))}
+      </Box>
+      {/* Channel rendering section */}
+    </Box>
+  );
+};
+
+export default Groups;
