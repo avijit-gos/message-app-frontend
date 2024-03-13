1e7fd458dc3cbff12105bf25a98419af47bd8e5a
unknown <avijit-gos>
2024-03-13 13:34:44 +0530

    Add Timeline design

--- src/Pages/Private/ChatMainPage/Components/Timeline.jsx
+++ src/Pages/Private/ChatMainPage/Components/Timeline.jsx
@@ -2,11 +2,10 @@
 
 import { Box } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { FaCaretDown } from "react-icons/fa";
-import Timline from "../../../../Config/timeline.json";
+import { FaRegClock } from "react-icons/fa";
+import { TbUsersGroup, TbUserCog, TbUserPlus } from "react-icons/tb";
 
 const Timeline = ({ timelines }) => {
-  console.log(Timline);
   const groupedData = timelines.reduce((result, item) => {
     const date = new Date(item.createdAt).toDateString();
     if (!result[date]) {
@@ -20,41 +19,77 @@ const Timeline = ({ timelines }) => {
     <Box className='timeline_card_container'>
       {/* Map over the keys of the grouped data object */}
       {Object.keys(groupedData).map((date) => (
-        <Box className='timeline_card_section' key={date}>
-          <Box className='main_marker'></Box>
-          <Box className='date_section'>
+        <Box className='time_card_section'>
+          <Box className='timeline_card'>
             <Box className='timeline_icon_section'>
-              <FaCaretDown className='timeline_icon' />
+              <FaRegClock className='timeline_icon' />
             </Box>
-            <Box>
-              <Box className='timeline_date'>{date}</Box>
-              <Box>
-                {groupedData[date].map((document) => (
-                  <Box className='timeline_inner_card' key={document._id}>
-                    <Box className='marker'></Box>
-                    <p className='timeline_message_section'>
-                      {document.cat === 1 ? (
-                        <>Group has been created</>
+            <Box className='timeline_date'>{date}</Box>
+          </Box>
+          <Box className='marker'></Box>
+          <Box className='timeline_message_section'>
+            {groupedData[date].map((document) => (
+              <Box className='timeline_inner_card' key={document._id}>
+                <p className='timeline_message'>
+                  {document.cat === 1 ? (
+                    <>
+                      {" "}
+                      <span className='timeline_message_icon'>
+                        <TbUsersGroup />
+                      </span>
+                      Group has been created
+                    </>
+                  ) : (
+                    <>
+                      {document.cat === 2 ? (
+                        <>
+                          {" "}
+                          <span className='timeline_message_icon'>
+                            <TbUserCog />
+                          </span>
+                          Group's profile image has been updated
+                        </>
                       ) : (
                         <>
-                          {document.cat === 2 ? (
-                            <>Group's profile image has been updated</>
+                          {document.cat === 3 ? (
+                            <>
+                              <span className='timeline_message_icon'>
+                                <TbUserCog />
+                              </span>
+                              Group name has been changed
+                            </>
                           ) : (
                             <>
-                              {document.cat === 3 ? (
-                                <>Group name has been changed</>
+                              {document.cat === 4 ? (
+                                <>
+                                  <span className='timeline_message_icon'>
+                                    <TbUserCog />
+                                  </span>
+                                  Group bio has been changed
+                                </>
                               ) : (
                                 <>
-                                  {document.cat === 4 ? (
-                                    <>Group bio has been changed</>
+                                  {document.cat === 5 ? (
+                                    <>
+                                      <span className='timeline_message_icon'>
+                                        <TbUserPlus />
+                                      </span>
+                                      <span className='timeline_username'>
+                                        {document.user.name}
+                                      </span>
+                                      new member added.
+                                    </>
                                   ) : (
                                     <>
-                                      {document.cat === 5 ? (
+                                      {document.cat === 6 ? (
                                         <>
+                                          <span className='timeline_message_icon'>
+                                            <TbUserPlus />
+                                          </span>
                                           <span className='timeline_username'>
                                             {document.user.name}
                                           </span>
-                                          new member added.
+                                          select as admin.
                                         </>
                                       ) : (
                                         <></>
@@ -67,11 +102,11 @@ const Timeline = ({ timelines }) => {
                           )}
                         </>
                       )}
-                    </p>
-                  </Box>
-                ))}
+                    </>
+                  )}
+                </p>
               </Box>
-            </Box>
+            ))}
           </Box>
         </Box>
       ))}
--- src/Pages/Private/Home/Components/Message/Components/Timeline.jsx
+++ src/Pages/Private/Home/Components/Message/Components/Timeline.jsx
@@ -1,9 +1,117 @@
 /** @format */
 
-import React from "react";
+import { Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { FaRegClock } from "react-icons/fa";
+import { TbUsersGroup, TbUserCog, TbUserPlus } from "react-icons/tb";
 
-const Timeline = ({ timeline }) => {
-  return <div>Timeline</div>;
+const Timeline = ({ timelines }) => {
+  const groupedData = timelines.reduce((result, item) => {
+    const date = new Date(item.createdAt).toDateString();
+    if (!result[date]) {
+      result[date] = [];
+    }
+    result[date].push(item);
+    return result;
+  }, {});
+
+  return (
+    <Box className='timeline_card_container'>
+      {/* Map over the keys of the grouped data object */}
+      {Object.keys(groupedData).map((date) => (
+        <Box className='time_card_section' key={date}>
+          <Box className='timeline_card'>
+            <Box className='timeline_icon_section'>
+              <FaRegClock className='timeline_icon' />
+            </Box>
+            <Box className='timeline_date'>{date}</Box>
+          </Box>
+          <Box className='marker'></Box>
+          <Box className='timeline_message_section'>
+            {groupedData[date].map((document) => (
+              <Box className='timeline_inner_card' key={document._id}>
+                <p className='timeline_message'>
+                  {document.cat === 1 ? (
+                    <>
+                      {" "}
+                      <span className='timeline_message_icon'>
+                        <TbUsersGroup />
+                      </span>
+                      Group has been created
+                    </>
+                  ) : (
+                    <>
+                      {document.cat === 2 ? (
+                        <>
+                          {" "}
+                          <span className='timeline_message_icon'>
+                            <TbUserCog />
+                          </span>
+                          Group's profile image has been updated
+                        </>
+                      ) : (
+                        <>
+                          {document.cat === 3 ? (
+                            <>
+                              <span className='timeline_message_icon'>
+                                <TbUserCog />
+                              </span>
+                              Group name has been changed
+                            </>
+                          ) : (
+                            <>
+                              {document.cat === 4 ? (
+                                <>
+                                  <span className='timeline_message_icon'>
+                                    <TbUserCog />
+                                  </span>
+                                  Group bio has been changed
+                                </>
+                              ) : (
+                                <>
+                                  {document.cat === 5 ? (
+                                    <>
+                                      <span className='timeline_message_icon'>
+                                        <TbUserPlus />
+                                      </span>
+                                      <span className='timeline_username'>
+                                        {document.user.name}
+                                      </span>
+                                      new member added.
+                                    </>
+                                  ) : (
+                                    <>
+                                      {document.cat === 6 ? (
+                                        <>
+                                          <span className='timeline_message_icon'>
+                                            <TbUserPlus />
+                                          </span>
+                                          <span className='timeline_username'>
+                                            {document.user.name}
+                                          </span>
+                                          select as admin.
+                                        </>
+                                      ) : (
+                                        <></>
+                                      )}
+                                    </>
+                                  )}
+                                </>
+                              )}
+                            </>
+                          )}
+                        </>
+                      )}
+                    </>
+                  )}
+                </p>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
 };
 
 export default Timeline;
